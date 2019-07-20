7e482ed86435b8d26f846826aef5a4d3d3cbfcb3
lizdoyle <lizdoyle1121@gmail.com>
2019-07-19 23:19:51 -0400

    channeling error

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,47 +1,47 @@
-// STEP 1: Create a header component.
-// -----------------------
-// Using a function create the component you see below:
-//
-//  <div class="header">
-//    <span class="date">SMARCH 28, 2019</span>
-//    <h1>Lambda Times</h1>
-//    <span class="temp">98°</span>
-//  </div >
-// And add it to the DOM in the .headerContainer component
+// // STEP 1: Create a header component.
+// // -----------------------
+// // Using a function create the component you see below:
+// //
+// //  <div class="header">
+// //    <span class="date">SMARCH 28, 2019</span>
+// //    <h1>Lambda Times</h1>
+// //    <span class="temp">98°</span>
+// //  </div >
+// // And add it to the DOM in the .headerContainer component
 
- function Header() {
+//  function Header() {
 
-    const header = document.createElement('div');
-    header.classList.add('header');
+//     const header = document.createElement('div');
+//     header.classList.add('header');
 
 
-    const date = document.createElement('span');
-    const headerTitle = document.createElement('h1');
-    const temp = document.createElement('span');
+//     const date = document.createElement('span');
+//     const headerTitle = document.createElement('h1');
+//     const temp = document.createElement('span');
 
     
-    date.textContent = "MARCH 28, 2019";
-    headerTitle.textContent = "Lambda Times";
-    temp.textContent = "98°";
+//     date.textContent = "MARCH 28, 2019";
+//     headerTitle.textContent = "Lambda Times";
+//     temp.textContent = "98°";
 
    
    
-    date.classList.add('date');
-    headerTitle.classList.add('h1');
-    temp.classList.add('temp');
+//     date.classList.add('date');
+//     headerTitle.classList.add('h1');
+//     temp.classList.add('temp');
 
     
-    header.appendChild('date');
-    header.appendChild('headerTitle');
-    header.appendChild('temp');
+//     header.appendChild('date');
+//     header.appendChild('headerTitle');
+//     header.appendChild('temp');
 
 
 
-return Header
-}
+// return Header
+// }
 
-const nav = document.querySelector('.header-container');
-nav.appendChild(Header());
+// const nav = document.querySelector('.header-container');
+// nav.appendChild(Header());
 
-console.log(nav);
+// console.log(nav);
 
