ab3bc34437eab3439360dbf43daa2edf7392cfbc
lizdoyle <lizdoyle1121@gmail.com>
2019-07-19 22:22:52 -0400

    started on header not showing up

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,37 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+function Header() {
+
+    const header = document.createElement('div');
+    const date = document.createElement('span');
+    const headerTitle = document.createElement('h1');
+    const temp = document.createElement('span');
+
+    
+    date.textContent = "MARCH 28, 2019";
+    headerTitle.textContent = "Lambda Times";
+    temp.textContent = "98°";
+
+   
+    header.classList.add('header');
+    date.classList.add('date');
+    headerTitle.classList.add('h1');
+    temp.classList.add('temp');
+
+    
+    header.appendChild('date');
+    header.appendChild('h1');
+    header.appendChild('temp');
+
+
+
+return Header
+}
+
+const header = document.querySelector('.header-container');
+header.appendChild(Header());
+// header.appendChild(Header());
+
+// Header(headerItems);
+// parent.appendChild(newMenu)
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -1,44 +1,44 @@
-// Step 2: Create Tabs
-// -----------------------
-// Using axios send a GET request to the address: https://lambda-times-backend.herokuapp.com/topics
-// Once the data is returned console.log it and review the structure.
-// Iterate over the topics creating a new Tab component and add it to the DOM
-// under the .topics element.
-//
-//  The tab component should look like this:
-//    <div class="tab">topic here</div>
+// // Step 2: Create Tabs
+// // -----------------------
+// // Using axios send a GET request to the address: https://lambda-times-backend.herokuapp.com/topics
+// // Once the data is returned console.log it and review the structure.
+// // Iterate over the topics creating a new Tab component and add it to the DOM
+// // under the .topics element.
+// //
+// //  The tab component should look like this:
+// //    <div class="tab">topic here</div>
 
-// Axios to pull the data
-axios.get('https://lambda-times-backend.herokuapp.com/topics')
-    .then(data => {
-        console.log('The Topics API is working Correctly', data)
-        const apiData = data.data;
-        tabs.appendChild(createTab(apiData));
+// // Axios to pull the data
+// axios.get('https://lambda-times-backend.herokuapp.com/topics')
+//     .then(data => {
+//         console.log('The Topics API is working Correctly', data)
+//         const apiData = data.data;
+//         tabs.appendChild(createTab(apiData));
 
-    } )
-    .catch(error => {
-        console.log("Error, not able to pull the Topics API Information", error);
-    })
-
-
-
-    const tabs = document.querySelector('.tabs');
+//     } )
+//     .catch(error => {
+//         console.log("Error, not able to pull the Topics API Information", error);
+//     })
 
 
-    const tabArray = [];
-    tabArray.forEach(tabLink =>  {
-        axios.get(`https://lambda-times-backend.herokuapp.com/${topics}`)
-        .then(data => {
-            console.log('The Topics API is working Correctly', data);
-            const apiData = data.data;
-            tabs.appendChild(createTab(apiData));
+
+//     const tabs = document.querySelector('.tabs');
+
+
+//     const tabArray = [];
+//     tabArray.forEach(tabLink =>  {
+//         axios.get(`https://lambda-times-backend.herokuapp.com/${topics}`)
+//         .then(data => {
+//             console.log('The Topics API is working Correctly', data);
+//             const apiData = data.data;
+//             tabs.appendChild(createTab(apiData));
             
-        })
-        .catch(err => {
-            console.log("Error, not able to pull the Topics API Information", err);
-        })
+//         })
+//         .catch(err => {
+//             console.log("Error, not able to pull the Topics API Information", err);
+//         })
 
-    })
+//     })
 
 
     
@@ -47,23 +47,23 @@ axios.get('https://lambda-times-backend.herokuapp.com/topics')
 
 
 
-    function createTab(topic) {
-        // structure
+//     function createTab(topic) {
+//         // structure
        
-        const tabTopic = document.createElement('div');
-        const tab = document.createElement('button');
-        const tabTitle = document.createElement('span');
+//         const tabTopic = document.createElement('div');
+//         const tab = document.createElement('button');
+//         const tabTitle = document.createElement('span');
 
 
-        // classlist
-        tabTopic.classList.add('topics');
-        tab.classList.add('tab');
+//         // classlist
+//         tabTopic.classList.add('topics');
+//         tab.classList.add('tab');
 
-        // text content
-        tabTopic.textContent = `${topic}`;
+//         // text content
+//         tabTopic.textContent = `${topic}`;
 
 
-        // append
+//         // append
         
-        return createTab
-    }
+//         return createTab
+//     }
